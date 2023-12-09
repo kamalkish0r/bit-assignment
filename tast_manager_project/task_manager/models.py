006c19bd2e2a8674436dd5882f49d62eb6eb5bc2
kamalkish0r <ikkamalkish0r@gmail.com>
2023-12-09 16:57:53 +0530

    1. Create Model

--- tast_manager_project/task_manager/models.py
+++ tast_manager_project/task_manager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.title
